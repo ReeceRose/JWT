c7e5845b2f761845b61d43083c8ec18ca80f682b
Reece Rose <reecerose18@gmail.com>
2018-11-09 13:48:03 -0500

    Fixed namespaces

--- Backend/API/Controllers/AuthenticationController.cs
+++ Backend/API/Controllers/AuthenticationController.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-namespace Backend.Controllers
+namespace API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
--- Backend/API/Program.cs
+++ Backend/API/Program.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 
-namespace Backend
+namespace API
 {
     public class Program
     {
