86a79ef77951e5172b72e627ccd4c3835f077c64
Reece Rose <reecerose18@gmail.com>
2018-11-29 13:56:41 -0500

    Added boostrap for styling

--- JWT.Website/src/main.js
+++ JWT.Website/src/main.js
@@ -2,10 +2,7 @@ import Vue from 'vue'
 import App from '@/App.vue'
 import router from '@/router'
 import store from '@/store/store.js'
-// import axios from '@/axios.js'
 
-// axios
-// axios.
 Vue.config.productionTip = false
 
 new Vue({
