a1d44ab14ce7e0a3859cb66e7410bf856245c5f3
Reece Rose <reecerose18@gmail.com>
2018-12-06 18:42:10 -0500

    Added body to message, removed manual email check

--- JWT.Infrastructure/Notifications/NotificationService.cs
+++ JWT.Infrastructure/Notifications/NotificationService.cs
@@ -2,6 +2,7 @@
 using MailKit.Net.Smtp;
 using Microsoft.Extensions.Configuration;
 using MimeKit;
+using MimeKit.Text;
 
 namespace JWT.Infrastructure.Notifications
 {
@@ -21,13 +22,15 @@ namespace JWT.Infrastructure.Notifications
             email.From.Add(new MailboxAddress(name: _configuration["SMTP:Name"], address: _configuration["SMTP:Email"]));
             email.To.Add(new MailboxAddress(name: toName, address: toEmailAddress));
             email.Subject = subject;
-            var body = new BodyBuilder {HtmlBody = message };
+            email.Body = new TextPart(TextFormat.Html)
+            {
+                Text = message
+            };
 
             using (var client = new SmtpClient())
             {
                 client.ServerCertificateValidationCallback = (sender, certificate, certChainType, errors) => true;
                 client.AuthenticationMechanisms.Remove("XOAUTH2");
-                var host = _configuration["SMTP:Host"];
                 await client.ConnectAsync(_configuration["SMTP:Host"], 587, false)
                     .ConfigureAwait(false);
                 await client.AuthenticateAsync(_configuration["SMTP:Username"], _configuration["SMTP:Password"])
