215960cb16033bb7c8855e1860dcba8810270d34
Reece Rose <reecerose18@gmail.com>
2018-12-05 20:29:23 -0500

    Added config file, added package, and added config options

--- JWT.API/Startup.cs
+++ JWT.API/Startup.cs
@@ -32,6 +32,7 @@ namespace JWT.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //var test = Configuration["ClientId"];
             services.AddApiVersioning();
 
             services.AddScoped<IdentityDbContext, IdentityDbContext>();
