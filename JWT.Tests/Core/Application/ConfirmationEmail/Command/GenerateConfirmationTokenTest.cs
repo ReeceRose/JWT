cf514284caf8d36d8cbe83f6b89da97810f12216
reecerose <reecerose18@gmail.com>
2018-12-11 21:33:02 -0500

    Added GenerateConfirmationToken test

--- JWT.Tests/Core/Application/ConfirmationEmail/Command/GenerateConfirmationTokenTest.cs
+++ JWT.Tests/Core/Application/ConfirmationEmail/Command/GenerateConfirmationTokenTest.cs
@@ -1,10 +1,46 @@
-﻿namespace JWT.Tests.Core.Application.ConfirmationEmail.Command
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using JWT.Application.ConfirmationEmail.Command;
+using JWT.Tests.Helpers;
+using Microsoft.AspNetCore.Identity;
+using Moq;
+using Xunit;
+
+namespace JWT.Tests.Core.Application.ConfirmationEmail.Command
 {
     public class GenerateConfirmationTokenTest
     {
+        public List<IdentityUser> Users { get; set; }
+
+        public Mock<UserManager<IdentityUser>> UserManager { get; }
         public GenerateConfirmationTokenTest()
         {
+            Users = new List<IdentityUser>()
+            {
+                new IdentityUser()
+                {
+                    Email = "test@test.ca",
+                    UserName = "test@test.ca",
+                    EmailConfirmed = false
+                }
+            };
+
+            UserManager = MockHelper.MockUserManager(Users);
+        }
+
+        [Fact]
+        public void GenerateConfirmationToken_ShouldReturnToken()
+        {
+            var handler = new GenerateConfirmationTokenCommandHandler(UserManager.Object);
+
+            UserManager.Setup(m => m.GenerateEmailConfirmationTokenAsync(It.IsAny<IdentityUser>())).Returns(Task.FromResult("1234567890"));
+     
+            var token = handler.Handle(new GenerateConfirmationTokenCommand(Users.First()), default(CancellationToken)).Result;
             
+            Assert.NotNull(token);
+            Assert.NotEmpty(token);
         }
     }
 }
