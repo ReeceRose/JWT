2ee42f95286114887d01d8f3dd14009e0dee463e
reecerose <reecerose18@gmail.com>
2018-12-13 22:06:34 -0500

    Added ConfirmUserEmail tests

--- JWT.Tests/Core/Application/User/Command/ConfirmUserEmail/ConfirmUserEmailTest.cs
+++ JWT.Tests/Core/Application/User/Command/ConfirmUserEmail/ConfirmUserEmailTest.cs
@@ -1,63 +1,67 @@
-﻿using System.Linq;
+﻿using System.Threading;
+using System.Threading.Tasks;
 using JWT.Application.User.Command.ConfirmUserEmail;
+using JWT.Application.User.Query.GetUserById;
+using JWT.Domain.Exceptions;
+using JWT.Tests.Helpers;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Moq;
 using Xunit;
 
 namespace JWT.Tests.Core.Application.User.Command.ConfirmUserEmail
 {
     public class ConfirmUserEmailTest
     {
-        public ConfirmUserEmailCommandValidator Validator { get; }
+        public Mock<IMediator> Mediator { get; }
+        public Mock<MockUserManager> UserManager { get; }
+        public ConfirmUserEmailCommandHandler Handler { get; }
 
         public ConfirmUserEmailTest()
         {
             // Arrange
-            Validator = new ConfirmUserEmailCommandValidator();
+            Mediator = new Mock<IMediator>();
+            UserManager = new Mock<MockUserManager>();
+            Handler = new ConfirmUserEmailCommandHandler(Mediator.Object, UserManager.Object);
         }
 
         [Theory]
-        [InlineData("1234567890")]
-        [InlineData("0")]
-        public void ConfirmationUserEmail_UserIdIsValid(string userId)
+        [InlineData("1234234", "0954368")]
+        [InlineData("0954368", "1234234")]
+        public void ConfirmUserEmail_SuccessfullyConfirmsEmail(string userId, string token)
         {
+            // Arrange
+            var requestedUser = new IdentityUser()
+            {
+                Id =  userId
+            };
+            Mediator.Setup(m => m.Send(It.IsAny<GetUserByIdQuery>(), default(CancellationToken))).Returns(Task.FromResult(requestedUser));
+            UserManager.Setup(u => u.ConfirmEmailAsync(It.IsAny<IdentityUser>(), It.IsAny<string>())).Returns(Task.FromResult(IdentityResult.Success));
+            var query = new ConfirmUserEmailCommand()
+            {
+                UserId = userId,
+                Token = token
+            };
             // Act
-            var result = Validator.Validate(new ConfirmUserEmailCommand() {Token = "1", UserId = userId});
+            var result = Handler.Handle(query, CancellationToken.None).Result;
             // Assert
-            Assert.True(result.IsValid);
+            Assert.True(result);
         }
 
         [Theory]
-        [InlineData("")]
-        [InlineData(null)]
-        public void ConfirmationUserEmail_UserIdIsInvalid(string userId)
+        [InlineData("1234234", "0954368")]
+        [InlineData("0954368", "1234234")]
+        public async Task ConfirmUser_ThrowsInvalidUserException(string userId, string token)
         {
-            // Act
-            var result = Validator.Validate(new ConfirmUserEmailCommand() { Token = "1", UserId = userId });
-            // Assert
-            Assert.Contains("User ID required", result.Errors.First().ErrorMessage);
-            Assert.False(result.IsValid);
-        }
-
-        [Theory]
-        [InlineData("1234567890")]
-        [InlineData("0")]
-        public void ConfirmationUserEmail_TokenIsValid(string token)
-        {
-            // Act
-            var result = Validator.Validate(new ConfirmUserEmailCommand() {Token = token, UserId = "1"});
-            // Assert
-            Assert.True(result.IsValid);
-        }
-
-        [Theory]
-        [InlineData("")]
-        [InlineData(null)]
-        public void ConfirmationUserEmail_TokenIsInvalid(string token)
-        {
-            // Act
-            var result = Validator.Validate(new ConfirmUserEmailCommand() { Token = token, UserId = "1" });
-            // Assert
-            Assert.Contains("Token required", result.Errors.First().ErrorMessage);
-            Assert.False(result.IsValid);
+            // Arrange
+            Mediator.Setup(m => m.Send(It.IsAny<GetUserByIdQuery>(), default(CancellationToken))).Returns(Task.FromResult((IdentityUser) null));
+            var query = new ConfirmUserEmailCommand()
+            {
+                UserId = userId,
+                Token = token
+            };
+            // Act / Assert
+            await Assert.ThrowsAsync<InvalidUserException>(() => Handler.Handle(query, CancellationToken.None));
         }
     }
 }
---
+++ JWT.Tests/Core/Application/User/Command/ConfirmUserEmail/ConfirmUserEmailValidationTest.cs
@@ -0,0 +1,63 @@
+﻿using System.Linq;
+using JWT.Application.User.Command.ConfirmUserEmail;
+using Xunit;
+
+namespace JWT.Tests.Core.Application.User.Command.ConfirmUserEmail
+{
+    public class ConfirmUserEmailValidationTest
+    {
+        public ConfirmUserEmailCommandValidator Validator { get; }
+
+        public ConfirmUserEmailValidationTest()
+        {
+            // Arrange
+            Validator = new ConfirmUserEmailCommandValidator();
+        }
+
+        [Theory]
+        [InlineData("1234567890")]
+        [InlineData("0")]
+        public void ConfirmUserEmail_UserIdIsValid(string userId)
+        {
+            // Act
+            var result = Validator.Validate(new ConfirmUserEmailCommand() {Token = "1", UserId = userId});
+            // Assert
+            Assert.True(result.IsValid);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public void ConfirmUserEmail_UserIdIsInvalid(string userId)
+        {
+            // Act
+            var result = Validator.Validate(new ConfirmUserEmailCommand() { Token = "1", UserId = userId });
+            // Assert
+            Assert.Contains("User ID required", result.Errors.First().ErrorMessage);
+            Assert.False(result.IsValid);
+        }
+
+        [Theory]
+        [InlineData("1234567890")]
+        [InlineData("0")]
+        public void ConfirmUserEmail_TokenIsValid(string token)
+        {
+            // Act
+            var result = Validator.Validate(new ConfirmUserEmailCommand() {Token = token, UserId = "1"});
+            // Assert
+            Assert.True(result.IsValid);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public void ConfirmUserEmail_TokenIsInvalid(string token)
+        {
+            // Act
+            var result = Validator.Validate(new ConfirmUserEmailCommand() { Token = token, UserId = "1" });
+            // Assert
+            Assert.Contains("Token required", result.Errors.First().ErrorMessage);
+            Assert.False(result.IsValid);
+        }
+    }
+}
