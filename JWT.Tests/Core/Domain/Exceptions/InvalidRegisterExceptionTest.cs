68f2d9005524cd5cb40e3a5cb16cdcb074b3fdde
reecerose <reecerose18@gmail.com>
2018-12-14 14:29:00 -0500

    Added exception tests

---
+++ JWT.Tests/Core/Domain/Exceptions/InvalidRegisterExceptionTest.cs
@@ -0,0 +1,30 @@
+﻿using JWT.Domain.Exceptions;
+using Xunit;
+
+namespace JWT.Tests.Core.Domain.Exceptions
+{
+    public class InvalidRegisterExceptionTest
+    {
+        [Fact]
+        public void InvalidRegisterException_ReturnsDefaultErrorMessage()
+        {
+            // Act
+            var error = new InvalidRegisterException();
+            // Assert
+            Assert.Equal("Failed to create account. Please try again", error.Message);
+            Assert.Equal(typeof(InvalidRegisterException), error.GetType());
+        }
+
+        [Theory]
+        [InlineData("Failed to create account")]
+        [InlineData("Error: Account creation failed")]
+        public void InvalidRegisterException_ReturnsProvidedErrorMessage(string errorMessage)
+        {
+            // Act
+            var error = new InvalidRegisterException(errorMessage);
+            // Assert
+            Assert.Equal(errorMessage, error.Message);
+            Assert.Equal(typeof(InvalidRegisterException), error.GetType());
+        }
+    }
+}
---
+++ JWT.Tests/Core/Domain/Exceptions/InvalidUserExceptionTest.cs
@@ -0,0 +1,30 @@
+﻿using JWT.Domain.Exceptions;
+using Xunit;
+
+namespace JWT.Tests.Core.Domain.Exceptions
+{
+    public class InvalidUserExceptionTest
+    {
+        [Fact]
+        public void InvalidRegisterException_ReturnsDefaultErrorMessage()
+        {
+            // Act
+            var error = new InvalidUserException();
+            // Assert
+            Assert.Equal("A user with this ID does not exist", error.Message);
+            Assert.Equal(typeof(InvalidUserException), error.GetType());
+        }
+
+        [Theory]
+        [InlineData("Unknown user")]
+        [InlineData("Error: Not a user")]
+        public void InvalidUserException_ReturnsProvidedErrorMessage(string errorMessage)
+        {
+            // Act
+            var error = new InvalidUserException(errorMessage);
+            // Assert
+            Assert.Equal(errorMessage, error.Message);
+            Assert.Equal(typeof(InvalidUserException), error.GetType());
+        }
+    }
+}
