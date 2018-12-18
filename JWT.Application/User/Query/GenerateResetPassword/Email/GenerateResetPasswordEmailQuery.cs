3dfc80de018ea34536faf1a0868d906d72dbc749
reecerose <reecerose18@gmail.com>
2018-12-18 16:47:56 -0500

    Added Validator tests for password reset

--- JWT.Application/User/Query/GenerateResetPassword/Email/GenerateResetPasswordEmailQuery.cs
+++ JWT.Application/User/Query/GenerateResetPassword/Email/GenerateResetPasswordEmailQuery.cs
@@ -4,6 +4,8 @@ namespace JWT.Application.User.Query.GenerateResetPassword.Email
 {
     public class GenerateResetPasswordEmailQuery : IRequest<bool>
     {
+        public GenerateResetPasswordEmailQuery(string email) => Email = email;
 
+        public string Email { get; }
     }
 }
--- JWT.Application/User/Query/GenerateResetPassword/Email/GenerateResetPasswordEmailQueryValidator.cs
+++ JWT.Application/User/Query/GenerateResetPassword/Email/GenerateResetPasswordEmailQueryValidator.cs
@@ -4,6 +4,11 @@ namespace JWT.Application.User.Query.GenerateResetPassword.Email
 {
     public class GenerateResetPasswordEmailQueryValidator : AbstractValidator<GenerateResetPasswordEmailQuery>
     {
-
+        public GenerateResetPasswordEmailQueryValidator()
+        {
+            RuleFor(c => c.Email)
+                .NotNull().WithMessage("Email is required")
+                .EmailAddress().WithMessage("Email is required");
+        }
     }
 }
