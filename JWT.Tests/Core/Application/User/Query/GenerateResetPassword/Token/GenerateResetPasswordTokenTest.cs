d40c8193f408a056a0e8cfc0f0a5ef52ce797b93
reecerose <reecerose18@gmail.com>
2018-12-18 17:05:29 -0500

    Added GenerateResetPasswordToken test

---
+++ JWT.Tests/Core/Application/User/Query/GenerateResetPassword/Token/GenerateResetPasswordTokenTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading;
+using JWT.Application.User.Query.GenerateResetPassword.Token;
+using Microsoft.AspNetCore.Identity;
+using Xunit;
+
+namespace JWT.Tests.Core.Application.User.Query.GenerateResetPassword.Token
+{
+    public class GenerateResetPasswordTokenTest
+    {
+        public GenerateResetPasswordTokenQueryHandler Handler { get; }
+
+        public GenerateResetPasswordTokenTest()
+        {
+            // Arrange
+            Handler = new GenerateResetPasswordTokenQueryHandler();
+        }
+
+        [Theory]
+        [InlineData("email", "token")]
+        public void GenerateResetPasswordToken_ReturnsValidToken(string email, string token)
+        {
+            // Arrange
+            var requestedUser = new IdentityUser()
+            {
+                Email = email
+            };
+            // Act
+            var returnedToken = Handler.Handle(new GenerateResetPasswordTokenQuery(requestedUser), CancellationToken.None).Result;
+            // Assert
+            Assert.Equal(returnedToken, token);
+        }
+    }
+}
