d8947b50fa66f8015e9614f2ab12b01aa17e6dcd
reecerose <reecerose18@gmail.com>
2018-12-31 12:12:24 -0500

    Added AddUserClaim test

--- JWT.Tests/Core/Application/User/Command/AddUserClaim/AddUserClaimTest.cs
+++ JWT.Tests/Core/Application/User/Command/AddUserClaim/AddUserClaimTest.cs
@@ -1,7 +1,62 @@
-﻿namespace JWT.Tests.Core.Application.User.Command.AddUserClaim
+﻿using System;
+using System.Security.Claims;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using JWT.Application.User.Command.AddUserClaim;
+using JWT.Application.User.Model;
+using JWT.Application.Utilities;
+using JWT.Domain.Entities;
+using JWT.Tests.Helpers;
+using Microsoft.AspNetCore.Identity;
+using Moq;
+using Xunit;
+
+namespace JWT.Tests.Core.Application.User.Command.AddUserClaim
 {
     public class AddUserClaimTest
     {
+        public Mock<MockUserManager> UserManager { get; }
+        public IMapper Mapper { get; }
+        public AddUserClaimCommandHandler Handler { get; }
 
+        public AddUserClaimTest()
+        {
+            // Arrange
+            UserManager = new Mock<MockUserManager>();
+            Mapper = new Mapper(new MapperConfiguration(cfg => cfg.AddProfile(new MappingProfile())));
+            Handler = new AddUserClaimCommandHandler(UserManager.Object, Mapper);
+        }
+        [Theory]
+        [InlineData("user@test.com", "key", "value")]
+        [InlineData("user@test.com", "test", "claim")]
+        public async Task AddUserClaim_ClaimAdded(string email, string key, string value)
+        {
+            // Arrange
+            var user = new ApplicationUserDto()
+            {
+                Email = email
+            };
+            UserManager.Setup(u => u.AddClaimAsync(It.IsAny<ApplicationUser>(), It.IsAny<Claim>())).ReturnsAsync(IdentityResult.Success);
+            // Act
+            var result = await Handler.Handle(new AddUserClaimCommand(user, key, value), CancellationToken.None);
+            // Assert
+            Assert.True(result);
+        }
+
+        [Theory]
+        [InlineData("user@test.com", null, "value")]
+        [InlineData("user@test.com", "key", null)]
+        public void AddUserClaim_ClaimNotAdded(string email, string key, string value)
+        {
+            // Arrange
+            var user = new ApplicationUserDto()
+            {
+                Email = email
+            };
+            UserManager.Setup(u => u.AddClaimAsync(It.IsAny<ApplicationUser>(), It.IsAny<Claim>())).ReturnsAsync(IdentityResult.Failed());
+            // Act / Assert
+            Assert.Throws<AggregateException>(() => Handler.Handle(new AddUserClaimCommand(user, key, value), CancellationToken.None).Result);
+        }
     }
 }
