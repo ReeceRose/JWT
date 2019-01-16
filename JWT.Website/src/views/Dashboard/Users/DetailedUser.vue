3f382460d280f3242c293ef1b6b93bc31004cbae
reecerose <reecerose18@gmail.com>
2019-01-16 13:14:33 -0500

    Started basics of user table.

--- JWT.Website/src/views/Dashboard/Users/DetailedUser.vue
+++ JWT.Website/src/views/Dashboard/Users/DetailedUser.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="pt-3">
         Detailed user
     </div>
 </template>
--- JWT.Website/src/views/Dashboard/Users/Index.vue
+++ JWT.Website/src/views/Dashboard/Users/Index.vue
@@ -1,8 +1,52 @@
 <template>
-    <div v-if="this.$route.name === 'userDashboard' ">
+    <div v-if="this.$route.name === 'userDashboard' " class="pt-3">
         Users
+        <table border="1">
+            <thead>
+                <tr>
+                    <td>Email</td>
+                    <td>Username</td>
+                    <td>Date Joined</td>
+                    <td>Email Confirmed</td>
+                    <td>Management</td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(user, index) in users" :key="index">
+                    <td>{{ user.username }}</td>
+                    <td>{{ user.email }}</td>
+                    <td>{{ user.dateJoined }}</td>
+                    <td>{{ user.email }}</td>
+                    <td>Edit | Delete</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
     <div v-else>
         <router-view></router-view>
     </div>
 </template>
+
+<script>
+export default {
+    name: 'UserDashboard',
+    data() {
+        return {
+            users: [
+                {
+                    username: 'username here',
+                    email: 'email here',
+                    dateJoined: 'date here',
+                    emailConfirmed: false
+                },
+                {
+                    username: 'username here 2',
+                    email: 'email here 2',
+                    dateJoined: 'date here 2',
+                    emailConfirmed: false
+                },
+            ]
+        }
+    }
+}
+</script>
