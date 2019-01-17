d0459e83e4a6aa81d3a0e6902d44ae414e617b55
reecerose <reecerose18@gmail.com>
2019-01-16 20:08:08 -0500

    Added boostrap classes to table

--- JWT.Website/src/views/Dashboard/Users/Index.vue
+++ JWT.Website/src/views/Dashboard/Users/Index.vue
@@ -1,22 +1,22 @@
 <template>
-    <div v-if="this.$route.name === 'userDashboard' " class="pt-3">
-        Users
-        <table border="1">
-            <thead>
+    <div v-if="this.$route.name === 'userDashboard' " class="pt-3 table-responsive">
+        <h2 class="text-center pb-4">Users</h2>
+        <table border="1" class="table table-bordered table-hover text-center">
+            <thead class="thead-dark">
                 <tr>
-                    <td>Email</td>
-                    <td>Username</td>
-                    <td>Date Joined</td>
-                    <td>Email Confirmed</td>
-                    <td>Management</td>
+                    <th class="header">Email</th>
+                    <th class="">Date Joined</th>
+                    <th class="">Email Confirmed</th>
+                    <th class="header">Account Enabled</th>
+                    <th class="header">Management</th>
                 </tr>
             </thead>
             <tbody>
                 <tr v-for="(user, index) in users" :key="index">
-                    <td>{{ user.username }}</td>
                     <td>{{ user.email }}</td>
                     <td>{{ user.dateJoined }}</td>
-                    <td>{{ user.email }}</td>
+                    <td>{{ user.accountEnabled }}</td>
+                    <td>{{ user.emailConfirmed }}</td>
                     <td>Edit | Delete</td>
                 </tr>
             </tbody>
@@ -34,15 +34,15 @@ export default {
         return {
             users: [
                 {
-                    username: 'username here',
                     email: 'email here',
                     dateJoined: 'date here',
+                    accountEnabled: false,
                     emailConfirmed: false
                 },
                 {
-                    username: 'username here 2',
                     email: 'email here 2',
                     dateJoined: 'date here 2',
+                    accountEnabled: false,
                     emailConfirmed: false
                 },
             ]
