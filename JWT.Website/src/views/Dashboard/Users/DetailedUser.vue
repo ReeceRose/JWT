aa0c7bd56b0c73ee88536ec4540c250e51b4433d
reecerose <reecerose18@gmail.com>
2019-01-16 22:07:20 -0500

    Added buttons for email confirmed and account enabled

--- JWT.Website/src/views/Dashboard/Users/DetailedUser.vue
+++ JWT.Website/src/views/Dashboard/Users/DetailedUser.vue
@@ -5,39 +5,40 @@
                 <h2 class="text-center pb-4">User</h2>
             </div>
         </div>
-        <NarrowCard>
-
-        </NarrowCard>
-        <div class="row">
-            <div class="col text-center">
-                <h5>Email: {{ user.email }}</h5>
+        <WideCard :title="user.email">
+            <div slot="card-content">
+                <div class="col-12">
+                    <ul>
+                        <li><span class="item">Date Joined: {{ user.dateJoined }}</span></li>
+                        <li>
+                            <span class="item" v-if="user.emailConfirmed">Email Confirmed</span>
+                            <span class="item" v-else><button class="btn btn-primary">Send Confirmation Email</button></span>
+                        </li>
+                        <li>
+                            <span class="item" v-if="user.accountEnabled"><button class="btn btn-primary">Disable Account</button></span>
+                            <span class="item" v-else><button class="btn btn-primary">Enable Account</button></span>
+                        </li>
+                    </ul>
+                </div>
             </div>
-        </div>
-        <div class="row">
-            <h5 class="">
-                Email: {{ user.email }}<br>
-                Date Joined: {{ user.dateJoined }}<br>
-                Account Enabled: {{ user.accountEnabled }}<br>
-                Email Confirmed: {{ user.emailConfirmed }}
-            </h5>
-        </div>
+        </WideCard>
     </div>
 </template>
 
 <script>
-import NarrowCard from '@/components/UI/Card/NarrowCard.vue'
+import WideCard from '@/components/UI/Card/WideCard.vue'
 
 export default {
     name: 'DetailedUser',
     components: {
-        NarrowCard
+        WideCard
     },
     data() {
         return {
             user: {
                     id: '123123',
-                    email: 'email here',
-                    dateJoined: 'date here',
+                    email: 'test@gmail.com',
+                    dateJoined: '12/12/2018',
                     accountEnabled: true,
                     emailConfirmed: true
                 }
@@ -45,3 +46,17 @@ export default {
     }
 }
 </script>
+
+<style lang="scss" scoped>
+ul {
+    list-style: none;
+
+    .item {
+        font-size: 1.2rem;
+
+        .btn {
+            margin: 5px 0;
+        }
+    }
+}
+</style>
