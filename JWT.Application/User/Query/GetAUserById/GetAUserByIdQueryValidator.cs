3213886d697e607382bdb8d0c0ec2f63fae2f161
reecerose <reecerose18@gmail.com>
2019-01-24 14:52:06 -0500

    Added GetAUserById validator

---
+++ JWT.Application/User/Query/GetAUserById/GetAUserByIdQueryValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace JWT.Application.User.Query.GetAUserById
+{
+    public class GetAUserByIdQueryValidator : AbstractValidator<GetAUserByIdQuery>
+    {
+        public GetAUserByIdQueryValidator()
+        {
+            RuleFor(c => c.UserId)
+                .NotEmpty().WithMessage("User ID required")
+                .NotNull().WithMessage("User ID required");
+        }
+    }
+}
--- JWT.Tests/Core/Application/User/Query/GenerateResetPassword/Token/GenerateResetPasswordTokenValidatorTest.cs
+++ JWT.Tests/Core/Application/User/Query/GenerateResetPassword/Token/GenerateResetPasswordTokenValidatorTest.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace JWT.Tests.Core.Application.User.Query.GenerateResetPassword.Token
+﻿namespace JWT.Tests.Core.Application.User.Query.GenerateResetPassword.Token
 {
     class GenerateResetPasswordTokenValidatorTest
     {
