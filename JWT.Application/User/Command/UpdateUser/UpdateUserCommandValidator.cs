0abb64773b9eb806bf46f946a65bd7b074db8fd8
reecerose <reecerose18@gmail.com>
2019-01-24 14:35:42 -0500

    Added Update User Validator

---
+++ JWT.Application/User/Command/UpdateUser/UpdateUserCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace JWT.Application.User.Command.UpdateUser
+{
+    public class UpdateUserCommandValidator : AbstractValidator<UpdateUserCommand>
+    {
+        public UpdateUserCommandValidator()
+        {
+            RuleFor(c => c.User)
+                .NotNull().WithMessage("User required");
+        }
+    }
+}
---
+++ JWT.Tests/Core/Application/User/Command/UpdateUser/UpdateUserValidatorTest.cs
@@ -0,0 +1,43 @@
+﻿using JWT.Application.User.Command.UpdateUser;
+using JWT.Domain.Entities;
+using Xunit;
+
+namespace JWT.Tests.Core.Application.User.Command.UpdateUser
+{
+    public class UpdateUserValidatorTest
+    {
+        public UpdateUserCommandValidator Validator { get; }
+
+        public UpdateUserValidatorTest()
+        {
+            // Arrange
+            Validator = new UpdateUserCommandValidator();
+        }
+
+        [Theory]
+        [InlineData("1234567890")]
+        [InlineData("0987654321")]
+        public void UpdateUser_UserIdIsValid(string userId)
+        {
+            // Arrange
+            var user = new ApplicationUser()
+            {
+                Id = userId
+            };
+            // Act
+            var result = Validator.Validate(new UpdateUserCommand(user));
+            // Assert
+            Assert.True(result.IsValid);
+        }
+
+        [Fact]
+        public void UpdateUser_UserIdIsInvalid()
+        {
+            // Act
+            var result = Validator.Validate(new UpdateUserCommand(null));
+            // Assert
+            Assert.False(result.IsValid);
+            Assert.Equal("User required", result.Errors[0].ErrorMessage);
+        }
+    }
+}
