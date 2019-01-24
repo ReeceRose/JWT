eb98622fa11e138491133d96f587a3835e1ef22e
reecerose <reecerose18@gmail.com>
2019-01-24 14:17:30 -0500

    Added Disable User Validator

---
+++ JWT.Application/User/Command/DisableUser/DisableUserCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace JWT.Application.User.Command.DisableUser
+{
+    public class DisableUserCommandValidator : AbstractValidator<DisableUserCommand>
+    {
+        public DisableUserCommandValidator()
+        {
+            RuleFor(c => c.UserId)
+               .NotEmpty().WithMessage("User ID required")
+               .NotNull().WithMessage("User ID required");
+
+        }
+    }
+}
