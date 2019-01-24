24875f6e06d4e09d224111f85c8f95132f8e6397
reecerose <reecerose18@gmail.com>
2019-01-24 14:48:09 -0500

    Adding more validators

---
+++ JWT.Application/User/Query/GenerateEmailConfirmation/Token/GenerateEmailConfirmationTokenQueryValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace JWT.Application.User.Query.GenerateEmailConfirmation.Token
+{
+    public class GenerateEmailConfirmationTokenQueryValidator : AbstractValidator<GenerateEmailConfirmationTokenQuery>
+    {
+        public GenerateEmailConfirmationTokenQueryValidator()
+        {
+            RuleFor(u => u.User)
+                .NotNull().WithMessage("User required");
+        }
+    }
+}
---
+++ JWT.Tests/Core/Application/User/Query/GenerateResetPassword/Token/GenerateResetPasswordTokenValidatorTest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JWT.Tests.Core.Application.User.Query.GenerateResetPassword.Token
+{
+    class GenerateResetPasswordTokenValidatorTest
+    {
+    }
+}
