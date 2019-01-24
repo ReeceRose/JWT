bca894f1182fd39c9e4f385d46a93598a4fd91c8
reecerose <reecerose18@gmail.com>
2019-01-24 15:01:28 -0500

    Added GetUserById validator

---
+++ JWT.Application/User/Query/GetUserById/GetUserByIdQueryValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace JWT.Application.User.Query.GetUserById
+{
+    public class GetUserByIdQueryValidator : AbstractValidator<GetUserByIdQuery>
+    {
+        public GetUserByIdQueryValidator()
+        {
+            RuleFor(c => c.UserId)
+                .NotEmpty().WithMessage("User ID required")
+                .NotNull().WithMessage("User ID required");
+        }
+    }
+}
